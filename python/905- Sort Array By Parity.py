e416941c3f2e94ec1fd8e48bf3299c6d55fb1dd8
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2019-11-24 23:59:30 -0500

    Update 905- Sort Array By Parity.py

--- python/905- Sort Array By Parity.py
+++ python/905- Sort Array By Parity.py
@@ -1,3 +1,4 @@
+#Solution One 
 class Solution:
     def sortArrayByParity(self, A):
         arr = [] 
@@ -9,3 +10,16 @@ class Solution:
             if (A[i] % 2 != 0): 
                 arr.append(A[i])
         return arr 
+
+    
+#solution Two 
+import collections 
+class Solution(object):
+    def sortArrayByParity(self, A):
+        de = collections.deque([])
+        for i in A: 
+            if i % 2 == 0:
+                de.appendleft(i)
+            else:
+                de.append(i)
+        return de 
