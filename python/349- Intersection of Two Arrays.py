20a399ed374c139d6303dccc3ebacd15d32e3a04
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2019-12-19 21:37:54 -0500

    Update 349- Intersection of Two Arrays.py

--- python/349- Intersection of Two Arrays.py
+++ python/349- Intersection of Two Arrays.py
@@ -1,3 +1,5 @@
+"ONE WAY TO SOLVE IT" 
+
 import collections 
 class Solution(object):
     def intersection(self, nums1, nums2):
@@ -8,3 +10,14 @@ class Solution(object):
             if b.values()[i] == 2: 
                 arr.append(b.keys()[i])
         return arr
+    
+    
+    
+" ANOTHER WAY" 
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        arr = [] 
+        for i in set(nums1): 
+            if i in set(nums2): 
+                arr.append(i)
+        return arr 
