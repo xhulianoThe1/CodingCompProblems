9c52fe8bc7f3517401ada6c8d047b85291b04f76
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2019-12-26 15:31:07 -0500

    Update 217- Contains Duplicate.py

--- python/217- Contains Duplicate.py
+++ python/217- Contains Duplicate.py
@@ -1,3 +1,4 @@
+## First solution ###
 class Solution(object):
     def containsDuplicate(self, nums):
         nums = sorted(nums)
@@ -7,3 +8,23 @@ class Solution(object):
                 return True 
             i+=1
         return False
+
+ ## Second Solution ##
+class Solution(object):
+    def containsDuplicate(self, nums):
+        arr = set()
+        for i in nums: 
+            if i in arr:
+                return True 
+            else: 
+                arr.add(i)
+        return False
+
+### Third Solution ###
+import collections 
+class Solution(object):
+    def containsDuplicate(self, nums):
+        for i in collections.Counter(nums).values(): 
+            if i > 1: 
+                return True
+        return False  
