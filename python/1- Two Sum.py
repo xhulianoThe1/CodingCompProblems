ecf088af7c80ec2e7421f48ef06a1ece6cac274d
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2019-12-26 15:39:11 -0500

    Update 1- Two Sum.py

--- python/1- Two Sum.py
+++ python/1- Two Sum.py
@@ -1,13 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
         dict = {}
-        #Use enumerate as a way to index 
         for index, val in enumerate(nums): 
-            if (target - val) in dict: 
-                #if this holds true we return the indexes 
+            if (target - val) in dict:  
                 return (index, dict[target-val])
             dict[val] = index
-        #error handling
         return 0
         
         
