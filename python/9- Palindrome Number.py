8b75fd2db642bf3e31a9bb79db8998e990e49a8a
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2020-02-27 17:02:50 -0500

    Update 9- Palindrome Number.py

--- python/9- Palindrome Number.py
+++ python/9- Palindrome Number.py
@@ -2,3 +2,14 @@ class Solution(object):
     def isPalindrome(self, x):
         x = str(x)
         return (x == x[::-1])
+  
+#Solution without converting to int 
+class Solution(object):
+    def isPalindrome(self, x):
+        res = 0 
+        s = abs(x)
+        while s!= 0: 
+            res*=10
+            res += s % 10 
+            s /= 10 
+        return res == x
