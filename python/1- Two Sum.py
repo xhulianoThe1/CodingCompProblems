6a93016a9ed6380ed7ad7ca8ede7ea530ddea945
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2022-01-10 15:04:41 -0800

    Update 1- Two Sum.py

--- python/1- Two Sum.py
+++ python/1- Two Sum.py
@@ -1,15 +1,3 @@
-#Solution 1 
-class Solution(object):
-    def twoSum(self, nums, target):
-        dic = {} 
-        for i in range(len(nums)): 
-            dic[nums[i]] = i 
-        for i in range(len(nums)): 
-            if target - nums[i] in dic and i != dic[target - nums[i]]: 
-                return [i, dic[target - nums[i]]] 
-
-
-#Solution 2 
 class Solution(object):
     def twoSum(self, nums, target):
         dict = {}
