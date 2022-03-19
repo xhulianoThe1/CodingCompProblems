e739a27b41b1ccbd3a073218c1e88cfa4cfd8f5b
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2022-03-18 18:37:50 -0700

    Update 49- Group Anagrams.py

--- python/49- Group Anagrams.py
+++ python/49- Group Anagrams.py
@@ -1,12 +1,10 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        arr = [] 
-        for i in range(len(strs)):
-            arr.append(("".join(sorted(strs[i])), strs[i]))
-        dic = collections. defaultdict(list)
-        for i in range(len(arr)): 
-            if arr[i][0] not in dic:
-                dic[arr[i][0]].append(arr[i][1]) 
-            else: 
-                dic[arr[i][0]].append(arr[i][1]) 
+class Solution(object):
+    def groupAnagrams(self, strs):
+        dic = {} 
+        for i in strs: 
+            s_str = ''.join(sorted(i))
+            dic[s_str] = []
+        for i in strs: 
+            s_str = ''.join(sorted(i))
+            dic[s_str].append(i)
         return dic.values()
