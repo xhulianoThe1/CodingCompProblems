ff6051b61eb84ed5f665cfc65efad78ce9cd2a90
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2022-03-19 11:53:25 -0700

    Update 242- Valid Anagram.py

--- python/242- Valid Anagram.py
+++ python/242- Valid Anagram.py
@@ -13,3 +13,21 @@ class Solution(object):
 class Solution(object):
 	def isAnagram(self, s, t):
 		sorted(s) == sorted(t) 
+
+#Solution 3 
+from collections import defaultdict 
+class Solution(object):
+    def isAnagram(self, s, t):
+        dic = defaultdict(int)
+        for i in s:
+            dic[i] +=1 
+        for i in t: 
+            dic[i] -= 1 
+        if max(dic.values()) > 0 or min(dic.values()) < 0: 
+            return False
+        return True
+	##Can also do the following: 
+        # for i in dic.values():
+        #     if i != 0: 
+        #         return False 
+        # return True
