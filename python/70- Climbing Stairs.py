de93df3d36460c73fcb83db50788b02bebcc4683
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2022-03-20 11:52:02 -0700

    Update 70- Climbing Stairs.py

--- python/70- Climbing Stairs.py
+++ python/70- Climbing Stairs.py
@@ -1,3 +1,17 @@
+#Solution 1 
+class Solution(object):
+    def climbStairs(self, n):
+        if n <= 1: 
+            return 1  
+        prev = 1
+        curr = 2 
+        for i in range(2, n): 
+            temp = curr
+            curr += prev
+            prev = temp
+        return curr
+    
+#Solution 2 
 class Solution(object):
     def climbStairs(self, n):
         if n == 0 or n == 1: 
