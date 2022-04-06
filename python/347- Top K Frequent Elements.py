b1f4fe1657869cbe45a3a29b416ef3c772e353da
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2022-04-06 11:32:26 -0700

    Update 347- Top K Frequent Elements.py

--- python/347- Top K Frequent Elements.py
+++ python/347- Top K Frequent Elements.py
@@ -1,9 +1,28 @@
-import collections
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        a = collections.Counter(nums).most_common()
-        i, arr = 0, []  
-        while i < k: 
-            arr.append(a[i][0]) 
-            i+= 1
-        return arr
+#Solution 1 
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = Counter(nums).most_common()
+        ans = []
+        for i in dic: 
+            if k!= 0:
+                ans.append(i[0])
+            else: 
+                break 
+            k-=1
+        return ans
+
+ #Solution 2 without calling most_common() 
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = Counter(nums)
+        ans = []
+        s_list = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+        for i in s_list: 
+            if k!= 0: 
+                ans.append(i[0])
+            else: 
+                break 
+            k-=1
+        return ans
