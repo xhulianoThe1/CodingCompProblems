f63320f1978d150b64f46e7c915496ce7e015f9b
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2022-04-23 13:13:52 -0700

    Update int_as_array_increment.py

--- EPI/int_as_array_increment.py
+++ EPI/int_as_array_increment.py
@@ -2,12 +2,11 @@ def plus_one(A: List[int]) -> List[int]:
     # naive one line solution 
     return [int(i) for i in str(int(''.join(list(map(str, A)))) + 1 )]
   
-  def plus_one(A: List[int]) -> List[int]:
-    #Kind of better solution but still not as clean
-    #as the one in the book :) 
+def plus_one(A: List[int]) -> List[int]:
+    #Faster solution but still not the greatest 
     if A[0] == 9 and len(A) ==1:
         return [1, 0]
-   
+    
     for i in reversed(range(len(A))): 
         if A[i] == 9 and A[i-1] != 9: 
             A[i] = 0 
@@ -24,10 +23,7 @@ def plus_one(A: List[int]) -> List[int]:
             A[i] = 0 
             A.insert(0, 1)
             break 
-        elif A[i] == 9: 
+        elif A[i] == 9 or (A[i] ==10 and i!= 0 and A[i-1] == 9): 
             A[i] = 0 
             A[i-1] +=1 
-        elif A[i] ==10 and i!= 0 and A[i-1] == 9: 
-            A[i] = 0 
-            A[i-1] += 1
     return A
