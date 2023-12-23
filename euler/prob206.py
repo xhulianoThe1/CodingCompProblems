b5ced56416a6bbdf3a3f33ae8c60ad45b14c46b3
Xhuliano Brace <34151358+xhulianoThe1@users.noreply.github.com>
2023-12-22 19:21:03 -0800

    Create prob206.py

---
+++ euler/prob206.py
@@ -0,0 +1,7 @@
+arr = [1, "", 2, "", 3, "", 4, "", 5, "", 6, "", 7, "", 8, "", 9, "", 0]
+for i in range(10**9, 10**10, 10): 
+    li = list(map(int, str(i**2)))
+    if li[0] == 1 and li[-1] == 0: 
+        if li[::2] == arr[::2] and len(li) == len(arr): 
+            print(i)
+            break
